0f43bd0ba8cd7ced214d79796656cb1be57d144b
manuelsaenz27 <manu_27_93@hotmail.com>
2022-11-03 12:35:41 -0300

    mx & sfx para gameplay, ganar, perder

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,7 +13,14 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID PLAY_GANAR_SCENE_CUE = 2412447196U;
+        static const AkUniqueID PLAY_HIT_BIBLE = 1848743998U;
+        static const AkUniqueID PLAY_HIT_BODY_BLOOD = 25700201U;
+        static const AkUniqueID PLAY_HIT_ESTANTE = 416173716U;
+        static const AkUniqueID PLAY_HIT_HOLY_WATER = 376588150U;
         static const AkUniqueID PLAY_MUSICA_ESCENE_150 = 2110865167U;
+        static const AkUniqueID PLAY_PERDER_SCENE_CUE = 287133965U;
+        static const AkUniqueID PLAY_UI_MENU_CUE_SCENE = 3490116095U;
     } // namespace EVENTS
 
     namespace BANKS
